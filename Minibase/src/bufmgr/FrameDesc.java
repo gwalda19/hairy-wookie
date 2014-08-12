c083b8066b5592358729907be7fb70fb8d0112e4
gwalda19 <barf2005@yahoo.com>
2014-08-11 23:59:44 -0400

    Update FrameDesc.java
    update comments

--- Minibase/src/bufmgr/FrameDesc.java
+++ Minibase/src/bufmgr/FrameDesc.java
@@ -6,7 +6,17 @@ import global.PageId;
  *
  *  @author David Shanline
  *  @author David Gwalthney
- *
+ * 
+ *  Each frame has certain states associated with it. These states include
+ *  whether the frame is dirty, whether it includes valid data (data which
+ *  reflects data in a disk page), and if it includes valid data then what
+ *  is the disk page number of the data, how many callers have pins on the
+ *  data (the pin count), and any other information you wish to store, for
+ *  example information relevant to the replacement algorithm.
+ * 
+ *  The below class stores the states as described above. This is the frame
+ *  table.
+ * 
  */
 class FrameDesc
 {
