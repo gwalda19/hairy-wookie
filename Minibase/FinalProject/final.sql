7cb4e6b53676737c2a2dba8f899381eebc5de3aa
gwalda19 <barf2005@yahoo.com>
2014-08-21 17:40:20 -0400

    Create final.sql

---
+++ Minibase/FinalProject/final.sql
@@ -0,0 +1,117 @@
+PRAGMA FOREIGN_KEY = ON;
+
+BEGIN TRANSACTION;
+
+DROP TABLE photo_users;
+DROP TABLE photo_files;
+DROP TABLE photo_user_links;
+DROP TABLE photo_comments;
+
+CREATE TABLE `photo_users` (
+  `user_id` int(6) NOT NULL,
+  `joindate` date,
+  `username` varchar(20),
+  `password` char(40),     
+  `profile_pic_id` int(8), 
+  PRIMARY KEY  (`user_id`)
+);
+
+CREATE TABLE `photo_files` (
+  `photo_id` int(8) NOT NULL,
+  `uploaddate` date,
+  `uploadname` varchar(128),
+  `caption` varchar(128),
+  `filelocation` varchar(256),
+  PRIMARY KEY  (`photo_id`)
+);
+
+CREATE TABLE `photo_user_links` (
+  `connection_id` int(8) NOT NULL,
+  `user_id` int(6),
+  `photo_id` int(8),
+  PRIMARY KEY  (`connection_id`)
+);
+
+CREATE TABLE `photo_comments` (
+  `comment_id` int(8) NOT NULL,
+  `user_id` int(6), 
+  `photo_id` int(8),
+  `comment_text` varchar(128),
+  PRIMARY KEY  (`comment_id`)
+);
+
+-- new tables------------------------------------------------------------
+CREATE TABLE `user_group` (
+  `group_id` int(6) NOT NULL,
+  `user_id` int(6), 
+  `joindate` date,
+  `groupname` varchar(20),  
+  `profile_pic_id` int(8), 
+  `about_text` varchar(128),
+  PRIMARY KEY  (`group_id`)
+);
+
+CREATE TABLE `group_comments` (
+  `comment_id` int(8) NOT NULL,
+  `user_id` int(6), 
+  `group_id` int(6),
+  `comment_text` varchar(128),
+  PRIMARY KEY  (`comment_id`)
+);
+
+CREATE TABLE `photo_like` (
+  `connection_id` int(8) NOT NULL,
+  `user_id` int(6),
+  `photo_id` int(8),
+  PRIMARY KEY  (`connection_id`)
+);
+
+CREATE TABLE `comment_like` (
+  `connection_id` int(8) NOT NULL,
+  `user_id` int(6),
+  `comment_id` int(8),
+  PRIMARY KEY  (`connection_id`)
+);
+
+CREATE TABLE `events` (
+  `event_id` int(6) NOT NULL,
+  `eventdate` date,
+  `eventname` varchar(20),
+  `user_id` int(6),    
+  `profile_pic_id` int(8), 
+  PRIMARY KEY  (`event_id`)
+);
+
+CREATE TABLE `event_comments` (
+  `comment_id` int(8) NOT NULL,
+  `user_id` int(6), 
+  `event_id` int(6),
+  `comment_text` varchar(128),
+  PRIMARY KEY  (`comment_id`)
+);
+
+-- make new user
+insert into photo_users values(0, "2014-09-09", "mike", "norris", "");
+insert into photo_users values(1, "2014-09-09", "sean", "fast", "");
+insert into photo_users values(2, "2014-09-09", "dave", "shaneline", "");
+insert into photo_users values(3, "2014-09-09", "bill", "annocki", "");
+
+  
+insert into photo_user_links values(0, "1", "1");
+insert into photo_user_links values(1, "1", "1");
+insert into photo_user_links values(2, "1", "1");
+
+
+-- make new photo file
+insert into photo_files values(0, "2013-09-09", "somepic.jpg", "this is a caption", "/some/path/to/the/photo/pic.jpg");
+
+
+-- make new photo_comments
+insert into photo_comments values(0, "1", "1", "This is a pic comment.");
+
+SELECT * FROM photo_users;
+SELECT * FROM photo_files;
+SELECT * FROM photo_user_links;
+SELECT * FROM photo_comments;
+
+COMMIT;
