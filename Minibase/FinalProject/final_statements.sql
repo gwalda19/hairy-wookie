546dc039acd50bbf6f2df0e773742127464cf30a
annockib <annockib@gmail.com>
2014-08-24 11:33:40 -0400

    Added UNIQUE to the friends and "like" tables.  You can only like a thing once and you can be friends to the same person only once.

--- Minibase/FinalProject/final_statements.sql
+++ Minibase/FinalProject/final_statements.sql
@@ -1,4 +1,3 @@
-
 -- make new user
 insert into photo_users values(0, "2014-09-09", "mike", "norris", "");
 insert into photo_users values(1, "2014-09-09", "sean", "fast", "");
@@ -6,13 +5,14 @@ insert into photo_users values(2, "2014-09-09", "dave", "shaneline", "");
 insert into photo_users values(3, "2014-09-09", "bill", "annocki", "");
 
   
-insert into photo_user_links values(0, "1", "1");
+insert into photo_user_links values(0, "1", "0");
 insert into photo_user_links values(1, "1", "1");
-insert into photo_user_links values(2, "1", "1");
+insert into photo_user_links values(2, "2", "1");
 
 
 -- make new photo file
-insert into photo_files values(0, "2013-09-09", "somepic.jpg", "this is a caption", "/some/path/to/the/photo/pic.jpg", 0);
+insert into photo_files values(0, "2013-09-09", "somepic.jpg", "this is a caption", "/some/path/to/the/photo/pic.jpg", NULL);
+insert into photo_files values(1, "2014-10-10", "mypic.jpg", "me", "/usr/pic.jpg", NULL);
 
 
 -- make new photo_comments
