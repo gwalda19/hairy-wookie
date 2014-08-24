0c77cb8aeee88b4c792bf6fe67c0a947df51e0ab
annockib <annockib@gmail.com>
2014-08-24 18:08:46 -0400

    Changed the int(6) and int(8) to more versatile integer.   Made the primary keys for the like and comment tables autoincrement because we don't care about providing the actual unique values.

--- Minibase/FinalProject/final.sql
+++ Minibase/FinalProject/final.sql
@@ -21,83 +21,79 @@ DROP TABLE IF EXISTS photo_albums;
 
 ------Users Tables------
 CREATE TABLE `photo_users` (
-  `user_id` int(6) NOT NULL,
+  `user_id` integer NOT NULL,
   `joindate` date,
   `username` varchar(20),
   `password` char(40),     
-  `profile_pic_id` int(8), 
+  `profile_pic_id` integer, 
   PRIMARY KEY (user_id),
   FOREIGN KEY (profile_pic_id) REFERENCES photo_files(photo_id)
 );
 
 CREATE TABLE `users_friend` (
-  `friend_id` int(6) NOT NULL,
-  `user_id` int(6),
-  `friend_user_id` int(6),
+  `friend_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer,
+  `friend_user_id` integer,
   UNIQUE (user_id, friend_user_id), 
-  PRIMARY KEY (friend_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (friend_user_id) REFERENCES photo_users(user_id)
 );
 
 ------Photo Tables------
 CREATE TABLE `photo_files` (
-  `photo_id` int(8) NOT NULL,
+  `photo_id` integer NOT NULL,
   `uploaddate` date,
   `uploadname` varchar(128),
   `caption` varchar(128),
   `filelocation` varchar(256),
-  `album_id` int(8),
+  `album_id` integer,
   PRIMARY KEY (photo_id)
   FOREIGN KEY (album_id) REFERENCES photo_albums(album_id)
 );
 
 CREATE TABLE `photo_user_links` (
-  `connection_id` int(8) NOT NULL,
-  `user_id` int(6),
-  `photo_id` int(8),
+  `connection_id` integer NOT NULL,
+  `user_id` integer,
+  `photo_id` integer,
   PRIMARY KEY (connection_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (photo_id) REFERENCES photo_files(photo_id)
 );
 
 CREATE TABLE `photo_comments` (
-  `comment_id` int(8) NOT NULL,
-  `user_id` int(6), 
-  `photo_id` int(8),
+  `comment_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer, 
+  `photo_id` integer,
   `comment_text` varchar(128),
-  PRIMARY KEY (comment_id),
   FOREIGN KEY(user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY(photo_id) REFERENCES photo_files(photo_id)
 );
 
 CREATE TABLE `photo_like` (
-  `connection_id` int(8) NOT NULL,
-  `user_id` int(6),
-  `photo_id` int(8),
+  `connection_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer,
+  `photo_id` integer,
   UNIQUE (user_id, photo_id), 
-  PRIMARY KEY  (`connection_id`),
   FOREIGN KEY(user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY(photo_id) REFERENCES photo_files(photo_id)
 );
 
 CREATE TABLE `comment_like` (
-  `connection_id` int(8) NOT NULL,
-  `user_id` int(6),
-  `comment_id` int(8),
+  `connection_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer,
+  `comment_id` integer,
   UNIQUE (user_id, comment_id), 
-  PRIMARY KEY (connection_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (comment_id) REFERENCES photo_comments(comment_id)
 );
 
 ------Groups Tables------
 CREATE TABLE `user_group` (
-  `group_id` int(6) NOT NULL,
-  `user_id` int(6), 
+  `group_id` integer NOT NULL,
+  `user_id` integer, 
   `joindate` date,
   `groupname` varchar(20),  
-  `profile_pic_id` int(8), 
+  `profile_pic_id` integer, 
   `about_text` varchar(128),
   PRIMARY KEY (group_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
@@ -105,85 +101,81 @@ CREATE TABLE `user_group` (
 );
 
 CREATE TABLE `group_comments` (
-  `comment_id` int(8) NOT NULL,
-  `user_id` int(6), 
-  `group_id` int(6),
+  `comment_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer, 
+  `group_id` integer,
   `comment_text` varchar(128),
-  PRIMARY KEY (comment_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (group_id) REFERENCES user_group(group_id)
 );
 
 CREATE TABLE `group_members` (
-  `group_member_id` int(8) NOT NULL,
+  `group_member_id` integer NOT NULL,
   `joindate` date,
-  `group_id` int(6),
-  `user_id` int(6),
+  `group_id` integer,
+  `user_id` integer,
   PRIMARY KEY (group_member_id),
   FOREIGN KEY (group_id) REFERENCES user_group(group_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id)
 );
 
 CREATE TABLE `group_comment_like` (
-  `group_comment_like_id` int(8) NOT NULL,
-  `user_id` int(6),
-  `group_comment_id` int(8),
+  `group_comment_like_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer,
+  `group_comment_id` integer,
   UNIQUE (user_id, group_comment_id), 
-  PRIMARY KEY (group_comment_like_id),
   FOREIGN KEY (group_comment_id) REFERENCES group_comments(comment_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id)
 );
 
 ------Events Tables------
 CREATE TABLE `events` (
-  `event_id` int(6) NOT NULL,
+  `event_id` integer NOT NULL,
   `eventdate` date,
   `eventname` varchar(20),
-  `user_id` int(6),    
-  `profile_pic_id` int(8), 
+  `user_id` integer,    
+  `profile_pic_id` integer, 
   PRIMARY KEY (event_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (profile_pic_id) REFERENCES photo_files(photo_id)
 );
 
 CREATE TABLE `event_comments` (
-  `comment_id` int(8) NOT NULL,
-  `user_id` int(6), 
-  `event_id` int(6),
+  `comment_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer, 
+  `event_id` integer,
   `comment_text` varchar(128),
-  PRIMARY KEY (comment_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (event_id) REFERENCES events(event_id)
 );
 
 CREATE TABLE `event_members` (
-  `event_members_id` int(8) NOT NULL,
+  `event_members_id` integer NOT NULL,
   `joindate` date,
-  `event_id` int(6),
-  `user_id` int(6),
+  `event_id` integer,
+  `user_id` integer,
   PRIMARY KEY(event_members_id),
   FOREIGN KEY(event_id) REFERENCES events(event_id),
   FOREIGN KEY(user_id) REFERENCES photo_users(user_id)
 );
 
 CREATE TABLE `event_comment_like` (
-  `event_comment_id` int(8) NOT NULL,
-  `user_id` int(6),
-  `comment_id` int(8),
+  `event_comment_id` integer PRIMARY KEY AUTOINCREMENT,
+  `user_id` integer,
+  `comment_id` integer,
   UNIQUE (user_id, comment_id), 
-  PRIMARY KEY (event_comment_id),
   FOREIGN KEY (user_id) REFERENCES photo_users(user_id),
   FOREIGN KEY (comment_id) REFERENCES event_comments(comment_id)
 );
 
 ------Albums Table------
 CREATE TABLE `photo_albums` (
-  `album_id` int(8) NOT NULL,
-  `user_id`  int(6),
+  `album_id` integer NOT NULL,
+  `user_id` integer,
   `name_of_album` varchar(128),
   `description` varchar(128),
   PRIMARY KEY (album_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-COMMIT;
+COMMIT TRANSACTION;
