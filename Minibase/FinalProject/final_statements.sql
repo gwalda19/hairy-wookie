d894986993b0e7176b6ce85f495b92c447ca4f2f
annockib <annockib@gmail.com>
2014-08-25 19:28:21 -0400

    Additional changes.

--- Minibase/FinalProject/final_statements.sql
+++ Minibase/FinalProject/final_statements.sql
@@ -1,29 +1,34 @@
 -- make new user
-insert into photo_users values(0, "2014-06-09", "Mike", "Norris", NULL);
-insert into photo_users values(1, "2013-09-09", "Sean", "Fast", NULL);
-insert into photo_users values(2, "2014-10-09", "Dave", "Shanline", NULL);
-insert into photo_users values(3, "2011-07-09", "Bill", "Annocki", NULL);
+insert into photo_users values(0, "2014-06-09", "Mike Norris", "password", NULL);
+insert into photo_users values(1, "2013-09-09", "Sean Fast", "password", NULL);
+insert into photo_users values(2, "2014-10-09", "Dave Shanline", "password", NULL);
+insert into photo_users values(3, "2011-07-09", "Bill Annocki", "password", NULL);
 
 
 -- make some friends
-insert into users_friend values(NULL, 1, 2);
-insert into users_friend values(NULL, 2, 2);
-insert into users_friend values(NULL, 0, 3);
+insert into users_friend values(1, 2);
+insert into users_friend values(2, 1);
+
+insert into users_friend values(2, 0);
+insert into users_friend values(0, 2);
+
+insert into users_friend values(0, 3);
+insert into users_friend values(3, 0);
   
-
-insert into photo_user_links values(0, 1, 0);
-insert into photo_user_links values(1, 1, 1);
-insert into photo_user_links values(2, 2, 1);
-
 
 -- make new photo file
 insert into photo_files values(0, "2013-09-09", "somepic.jpg", "this is a caption", "/some/path/to/the/photo/pic.jpg", NULL);
 insert into photo_files values(1, "2014-10-10", "mypic.jpg", "me", "/usr/pic.jpg", NULL);
 
 
+-- link users to photos
+insert into photo_user_links values(1, 0);
+insert into photo_user_links values(2, 1);
+
+
 -- make new photo likes
-insert into photo_like values(NULL, 2, 1);
-insert into photo_like values(NULL, 3, 1);
+insert into photo_like values(2, 1);
+insert into photo_like values(3, 1);
 
 
 -- make new photo comments
@@ -31,10 +36,15 @@ insert into photo_comments values(NULL, 0, 1, "This is a pic comment.");
 insert into photo_comments values(NULL, 1, 1, "This is another pic comment.");
 
 
+-- make some photo comment likes
+insert into comment_like values(2, 1);
+insert into comment_like values(0, 1);
+
 SELECT * FROM photo_users;
 SELECT * FROM photo_files;
 SELECT * FROM photo_user_links;
 SELECT * FROM photo_like;
 SELECT * FROM photo_comments;
+SELECT * FROM comment_like;
 SELECT * FROM users_friend;
 
