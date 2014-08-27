51a7d96c913d6ab5b4a9fa06e5cab716adaff0db
annockib <annockib@gmail.com>
2014-08-26 20:17:23 -0400

    Added some group info

--- Minibase/FinalProject/final_statements.sql
+++ Minibase/FinalProject/final_statements.sql
@@ -1,8 +1,8 @@
--- make new user
-insert into photo_users values(NULL, "2014-06-09", "Mike Norris", "password", NULL);
-insert into photo_users values(NULL, "2013-09-09", "Sean Fast", "password", NULL);
-insert into photo_users values(NULL, "2014-10-09", "Dave Shanline", "password", NULL);
-insert into photo_users values(NULL, "2011-07-09", "Bill Annocki", "password", NULL);
+-- make new users
+insert into photo_users (joindate, username, password) values ("2014-06-09", "Mike Norris", "password");
+insert into photo_users (joindate, username, password) values ("2013-09-09", "Sean Fast", "password");
+insert into photo_users (joindate, username, password) values ("2014-10-09", "Dave Shanline", "password");
+insert into photo_users (joindate, username, password) values ("2011-07-09", "Bill Annocki", "password");
 
 
 -- make some friends
@@ -17,28 +17,41 @@ insert into users_friend values(3, 0);
   
 
 -- make new photo file
-insert into photo_files values(NULL, "2013-09-09", "somepic.jpg", "this is a caption", "/some/path/to/the/photo/pic.jpg", NULL);
-insert into photo_files values(NULL, "2014-10-10", "mypic.jpg", "me", "/usr/pic.jpg", NULL);
+insert into photo_files (uploaddate, uploadname, caption, filelocation, owner_id) values ("2013-09-09", "somepic", "this is a caption", "/some/path/to/the/photo/somepic.jpg", 1);
+insert into photo_files (uploaddate, uploadname, caption, filelocation, owner_id) values ("2014-10-10", "mypic", "me", "/home/mypic.jpg", 2);
+insert into photo_files (uploaddate, uploadname, caption, filelocation, owner_id) values ("2012-07-23", "apic", "a thing", "/apic.jpg", 2);
+insert into photo_files (uploaddate, uploadname, caption, filelocation, owner_id) values ("2013-08-29", "pic", "you", "/usr/bin/pic.jpg", 3);
 
 -- make new photo likes
 insert into photo_like values(2, 1);
 insert into photo_like values(3, 1);
+insert into photo_like values(3, 2);
 
 
 -- make new photo comments
-insert into photo_comments values(NULL, 0, 1, "This is a pic comment.");
-insert into photo_comments values(NULL, 1, 1, "This is another pic comment.");
+insert into photo_comments (user_id, photo_id, comment_text) values (0, 1, "This is a pic comment.");
+insert into photo_comments (user_id, photo_id, comment_text) values (1, 1, "This is another pic comment.");
+insert into photo_comments (user_id, photo_id, comment_text) values (2, 3, "I love it");
 
 
 -- make some photo comment likes
-insert into comment_like values(2, 1);
-insert into comment_like values(0, 1);
+insert into comment_like values(2, 2);
+insert into comment_like values(0, 2);
+insert into comment_like values(1, 1);
+
+
+-- make a group and add some members
+insert into user_group (founder_id, foundingdate, groupname, about_text) values (2, "2011-07-09", "YOLO", "You only live once");
+insert into group_members (group_id, user_id, joindate) values (0, 0, "2014-08-26");
+insert into group_members (group_id, user_id, joindate) values (0, 2, "2014-08-26");
+
 
 SELECT * FROM photo_users;
+SELECT * FROM users_friend;
 SELECT * FROM photo_files;
-SELECT * FROM photo_user_links;
 SELECT * FROM photo_like;
 SELECT * FROM photo_comments;
 SELECT * FROM comment_like;
-SELECT * FROM users_friend;
+SELECT * FROM user_group;
+SELECT * FROM group_members;
 
