b77ae730f8da872542479f00b863b5a34cb4cb28
David <David@David-Laptop>
2014-08-28 01:46:39 -0400

    d

--- Minibase/FinalProject/final_statements.sql
+++ Minibase/FinalProject/final_statements.sql
@@ -100,28 +100,28 @@ update photo_files  set  album_id = 2 where photo_id = 7;
 update photo_files  set  album_id = 2 where photo_id = 8;
 
 
-SELECT * FROM photo_users;
-SELECT * FROM users_friend;
-SELECT * FROM photo_files;
-SELECT * FROM photo_like;
-SELECT * FROM photo_comments;
-SELECT * FROM comment_like;
-SELECT * FROM user_group;
-SELECT * FROM group_members;
-SELECT * FROM group_comments;
-SELECT * FROM group_comment_like;
-SELECT * FROM events;
-SELECT * FROM event_members;
-SELECT * FROM event_comments;
-SELECT * FROM event_comment_like;
-SELECT * FROM photo_albums;
+--SELECT * FROM photo_users;
+--SELECT * FROM users_friend;
+--SELECT * FROM photo_files;
+--SELECT * FROM photo_like;
+--SELECT * FROM photo_comments;
+--SELECT * FROM comment_like;
+--SELECT * FROM user_group;
+--SELECT * FROM group_members;
+--SELECT * FROM group_comments;
+--SELECT * FROM group_comment_like;
+--SELECT * FROM events;
+--SELECT * FROM event_members;
+--SELECT * FROM event_comments;
+--SELECT * FROM event_comment_like;
+--SELECT * FROM photo_albums;
 
 --List all photos that user Sean has posted. (working)
-SELECT photo_users.username, photo_files.uploadname
-FROM photo_users
-INNER JOIN photo_files
-ON photo_users.user_id = photo_files.owner_id
-WHERE photo_users.username = "Sean Fast";
+--SELECT photo_users.username, photo_files.uploadname
+--FROM photo_users
+--INNER JOIN photo_files
+--ON photo_users.user_id = photo_files.owner_id
+--WHERE photo_users.username = "Sean Fast";
 
 -- List all photos that user Sean has posted in album Cats.
 --SELECT photo_users.username, photo_files.uploadname, photo_albums.name_of_album
@@ -129,52 +129,55 @@ WHERE photo_users.username = "Sean Fast";
 --WHERE photo_users.user_id = photo_files.owner_id AND photo_users.username = "Sean Fast" AND photo_albums.name_of_album = "Cats";
 
 --List all comments user Dave left on photo somepic.  (working)
-SELECT photo_users.username, photo_files.uploadname, photo_comments.comment_text
-FROM photo_users
-INNER JOIN photo_files
-INNER JOIN photo_comments
-ON  photo_comments.photo_id = photo_files.photo_id AND photo_comments.user_id = photo_users.user_id
-WHERE photo_users.username = "Dave Shanline" AND photo_files.uploadname = "somepic";
+--SELECT photo_users.username, photo_files.uploadname, photo_comments.comment_text
+--FROM photo_users
+--INNER JOIN photo_files
+--INNER JOIN photo_comments
+--ON  photo_comments.photo_id = photo_files.photo_id AND photo_comments.user_id = photo_users.user_id
+--WHERE photo_users.username = "Dave Shanline" AND photo_files.uploadname = "somepic";
 
 --List all users in group YOLO that are attending event Final Project Presentation. (working)
-SELECT photo_users.username, user_group.groupname, events.eventname
-FROM photo_users
-INNER JOIN user_group
-INNER JOIN events
-INNER JOIN group_members
-INNER JOIN event_members
-ON photo_users.user_id = group_members.user_id AND photo_users.user_id = event_members.user_id
-WHERE user_group.groupname = "YOLO" AND events.eventname = "Final Project Presentation";
-
---List all users that are in group YOLO that are also friends.
---SELECT photo_users.username, user_group.groupname, users_friend.friend_user_id
+--SELECT photo_users.username, user_group.groupname, events.eventname
 --FROM photo_users
 --INNER JOIN user_group
+--INNER JOIN events
 --INNER JOIN group_members
---INNER JOIN users_friend
---ON photo_users.user_id = group_members.user_id AND photo_users.user_id = users_friend.user_id
---WHERE user_group.groupname = "YOLO";
+--INNER JOIN event_members
+--ON photo_users.user_id = group_members.user_id AND photo_users.user_id = event_members.user_id
+--WHERE user_group.groupname = "YOLO" AND events.eventname = "Final Project Presentation";
+
+--List all users that are in group YOLO that are also friends.
+SELECT photo_users.username, user_group.groupname, temp_photo_users.username
+FROM photo_users, photo_users temp_photo_users
+INNER JOIN user_group
+INNER JOIN group_members
+INNER JOIN users_friend
+ON     photo_users.user_id = group_members.user_id
+   AND photo_users.user_id = users_friend.user_id
+   AND users_friend.friend_user_id = group_members.user_id
+   AND users_friend.friend_user_id = temp_photo_users.user_id
+WHERE user_group.groupname = "YOLO";
 
 --List all users that are friends of Sean Fast. (working)
-SELECT photo_users.username, temp_photo_users.username
-FROM photo_users, photo_users temp_photo_users
-INNER JOIN users_friend
-ON photo_users.user_id = users_friend.user_id AND users_friend.friend_user_id = temp_photo_users.user_id
-WHERE photo_users.username = "Sean Fast";
+--SELECT photo_users.username, temp_photo_users.username
+--FROM photo_users, photo_users temp_photo_users
+--INNER JOIN users_friend
+--ON photo_users.user_id = users_friend.user_id AND users_friend.friend_user_id = temp_photo_users.user_id
+--WHERE photo_users.username = "Sean Fast";
 
 --List all items that user Bill Annocki has liked. (working, but double check)
-SELECT  photo_users.username   , photo_like.photo_id,
-        comment_like.comment_id, group_comment_like.group_comment_id,
-        event_comment_like.comment_id
-FROM photo_users
-INNER JOIN photo_like
-INNER JOIN comment_like
-INNER JOIN group_comment_like
-INNER JOIN event_comment_like
-ON     photo_users.user_id = photo_like.user_id
-   AND photo_users.user_id = comment_like.user_id
-   AND photo_users.user_id = group_comment_like.user_id
-   AND photo_users.user_id = event_comment_like.user_id
-WHERE photo_users.username = "Bill Annocki";
+--SELECT  photo_users.username   , photo_like.photo_id,
+--        comment_like.comment_id, group_comment_like.group_comment_id,
+--        event_comment_like.comment_id
+--FROM photo_users
+--INNER JOIN photo_like
+--INNER JOIN comment_like
+--INNER JOIN group_comment_like
+--INNER JOIN event_comment_like
+--ON     photo_users.user_id = photo_like.user_id
+--   AND photo_users.user_id = comment_like.user_id
+--   AND photo_users.user_id = group_comment_like.user_id
+--   AND photo_users.user_id = event_comment_like.user_id
+--WHERE photo_users.username = "Bill Annocki";
 
 
